f4b63cf0e4477c90dadf1d0ed8ff6ff66577302c
walterlong <327519632@qq.com>
2018-10-21 23:00:41 +0800

    vue start

--- src/main/java/com/gutongxue/controller/TestController.java
+++ src/main/java/com/gutongxue/controller/TestController.java
@@ -31,4 +31,25 @@ public class TestController {
         destMap.put("age", 32);
         return new Result(Result.STATE_SUCCESS, Result.INFO_SUCCESS, destMap);
     }
+    @RequestMapping("/goVue")
+    public String goVue(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/WEB-INF/html/home.html").forward(request, response);
+        return "html/vue";
+    }
+    @RequestMapping("/goFirstVueDemo")
+    public String goFirstVueDemo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/WEB-INF/html/home.html").forward(request, response);
+        return "html/firstVueDemo";
+    }
+
+    /**
+     *  Vue的监听属性demo  watch
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/goVueWatch")
+    public String goVueWatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return "html/VueWatch";
+    }
 }
